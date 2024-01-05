a871d6e5a605fbad74697b99a2c1dd3eda9bd4ad
gormkrtchyann <gormkrtchyann@gmail.com>
2024-01-05 14:07:51 +0400

    2

--- src/components/CoverVideo.js
+++ src/components/CoverVideo.js
@@ -19,7 +19,7 @@ video{
 const CoverVideo = () => {
   return (
     <VideoContainer>
-        <video src={GIF} type="video/mp4" autoPlay muted loop  />
+        <video src={GIF} type="video/mp4" autoPlay muted loop controls={false}  />
     </VideoContainer>
   )
 }
